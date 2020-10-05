9b3bd616e532e906d7f1f10cac2099220a17f1f7
Skye Breeze <skyexwarren@gmail.com>
2020-10-05 22:49:09 +0100

    First Commit

---
+++ JavaScript/Charging_Calculator.js
@@ -0,0 +1,9 @@
+
+class Car {
+    constructor(registration, currentrange, milesneeded, leavingtime) {
+        this.registration = registration;
+        this.currentrange = currentrange;
+        this.milesneeded = milesneeded;
+        this.leavingtime = leavingtime;
+    }
+}
